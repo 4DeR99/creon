9a2164a2a380ca3b090bd8d4a52b5658b74a282e
4DeR99 <moulmado@student.1337.ma>
2024-05-02 09:54:53 +0100

    chore: tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     colors: {
-      background: "#000",
-      blue: "#3D8BFF",
+      black: '#000',
+      blue: '#3D8BFF',
+      purple: '#AB23FF',
+      primaryText: '#fff',
     },
     fontFamily: {
-      monument: ["MonumentExtended-Regular", "sans-serif"],
-      satoshi: ["Satoshi-Regular", "sans-serif"],
-      satoshiBold: ["Satoshi-Bold", "sans-serif"],
+      monument: ['MonumentExtended-Regular', 'sans-serif'],
+      satoshi: ['Satoshi-Regular', 'sans-serif'],
+      satoshiBold: ['Satoshi-Bold', 'sans-serif'],
     },
     extend: {},
   },
