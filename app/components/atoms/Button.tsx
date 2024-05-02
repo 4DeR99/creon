b8d247ddfa18efa8370a65bb49acce18f70ed3f4
4DeR99 <moulmado@student.1337.ma>
2024-05-02 11:43:38 +0100

    feat: new component Button

---
+++ app/components/atoms/Button.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+interface ButtonProps {
+  title: string;
+  width?: string;
+  height?: string;
+  border?: string;
+  backGroundColor?: string;
+  hoverBackGroundColor?: string;
+  onClick: () => void;
+}
+
+function Button({
+  title,
+  width,
+  height,
+  border,
+  backGroundColor,
+  hoverBackGroundColor,
+  onClick,
+}: ButtonProps) {
+  const className = `relative overflow-hidden group rounded font-satoshiBold text-[18px] ${width} ${height} ${border} ${backGroundColor} ${hoverBackGroundColor} hover:border-none transition duration-500`;
+
+  return (
+    <button className={className} onClick={() => onclick}>
+      <span className="absolute top-0 left-0 w-full h-full flex justify-center ease-in-out items-center text-primaryText transition duration-[400ms] group-hover:translate-y-[-80%]">
+        {title}
+      </span>
+      <span className="absolute top-[80%] left-0 w-full h-full flex justify-center items-center ease-in-out text-primaryText transition duration-[400ms] group-hover:translate-y-[-80%]">
+        {title}
+      </span>
+    </button>
+  );
+}
+
+export default Button;
