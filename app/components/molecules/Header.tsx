2ed690a4468eb823cfe0f6bc24e2078120128425
4DeR99 <moulmado@student.1337.ma>
2024-05-02 11:44:01 +0100

    feat: Header modificationis

--- app/components/molecules/Header.tsx
+++ app/components/molecules/Header.tsx
@@ -1,7 +1,9 @@
+'use client';
 import React from 'react';
 import Image from 'next/image';
 import Logo from 'assets/icons/logo.svg';
 import HeaderItem from 'components/atoms/HeaderItem';
+import Button from 'components/atoms/Button';
 
 function Header() {
   const headerItems = [
@@ -12,12 +14,23 @@ function Header() {
   ];
 
   return (
-    <div className="w-full py-[30px] px-[50px] flex items-center justify-between border-[1px]">
+    <div className="w-full py-[30px] px-[50px] flex items-center justify-between">
       <Image src={Logo} alt="hamid" />
-      <div className="flex gap-10">
-        {headerItems.map((item, index) => (
-          <HeaderItem key={index} title={item.title} soon={item.soon} />
-        ))}
+      <div className="flex gap-[58px] items-center">
+        <div className="flex gap-10">
+          {headerItems.map((item, index) => (
+            <HeaderItem key={index} title={item.title} soon={item.soon} />
+          ))}
+        </div>
+        <Button
+          title="Connect"
+          width="w-[132px]"
+          height="h-[44px]"
+          border="border-[2px] border-[#fff]"
+          backGroundColor=""
+          hoverBackGroundColor="hover:bg-blue"
+          onClick={() => {}}
+        />
       </div>
     </div>
   );
