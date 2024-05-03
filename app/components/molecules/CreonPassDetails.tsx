456a7d5c3cccf2edae5bb7ea68e99e34183da760
4DeR99 <moulmado@student.1337.ma>
2024-05-03 23:20:35 +0100

    chore: split code to multiple files

---
+++ app/components/molecules/CreonPassDetails.tsx
@@ -0,0 +1,58 @@
+import Button from 'components/atoms/Button';
+import Card from 'components/atoms/Card';
+import LargeTitle from 'components/atoms/LargeTitle';
+import MediumTitle from 'components/atoms/MediumTitle';
+import Seperator from 'components/atoms/Seperator';
+import Image from 'next/image';
+import React from 'react';
+
+function CreonPassDetails() {
+  const largeTitle = 'creon pass nft';
+  const mediumTitle =
+    'The Creon NFT pass unlocks access to AI projects, the Creon launchpad, and a ticket to generate passive income through AI-driven tools';
+  const creonPassItems = [
+    'Pre-launch investment opportunities for upcoming AI projects',
+    'Free and early access to Creon built AI projects',
+    'Higher allocation limits on the Creon AI Launchpad',
+    'Revenue share distribution from Creon built AI projects',
+  ];
+
+  return (
+    <div className="flex flex-col items-start grow gap-8 py-4 ">
+      <LargeTitle title={largeTitle} />
+      <Seperator />
+      <MediumTitle
+        title={mediumTitle}
+        className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
+      />
+      <div className="flex justify-between w-full">
+        <div className="flex-col">
+          <div className="flex flex-col items-start gap-2 mt-[8px]">
+            {creonPassItems.map((item, index) => (
+              <Card key={index}>
+                <span className="font-satoshi text-[18px]">{item}</span>
+              </Card>
+            ))}
+          </div>
+          <Button
+            title="Buy Creon Pass"
+            height="h-[54px]"
+            width="w-full lg:w-[453px]"
+            backGroundColor="bg-gradient-to-r from-primary to-secondary"
+            hoverBackGroundColor="hover:to-primary"
+            className="my-10"
+          />
+        </div>
+        <Image
+          src="/images/creonPass.png"
+          alt="creonPass"
+          width={408}
+          height={408}
+          className="hidden xl:flex 2xl:hidden"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default CreonPassDetails;
--- app/components/organisms/CreonPass.tsx
+++ app/components/organisms/CreonPass.tsx
@@ -1,45 +1,20 @@
-import Card from 'components/atoms/Card';
-import LargeTitle from 'components/atoms/LargeTitle';
-import MediumTitle from 'components/atoms/MediumTitle';
-import Seperator from 'components/atoms/Seperator';
+import CreonPassDetails from 'components/molecules/CreonPassDetails';
 import Image from 'next/image';
 import React from 'react';
 
 function CreonPass() {
-  const largeTitle = 'creon pass nft';
-  const mediumTitle =
-    'The Creon NFT pass unlocks access to AI projects, the Creon launchpad, and a ticket to generate passive income through AI-driven tools';
-  const creonPassItems = [
-    'Pre-launch investment opportunities for upcoming AI projects',
-    'Free and early access to Creon built AI projects',
-    'Higher allocation limits on the Creon AI Launchpad',
-    'Revenue share distribution from Creon built AI projects',
-  ];
-
   return (
-    <div className="container w-[80%] md:w-[70%] lg:w-[750px] xl:w-[1075px] 2xl:w-[1850px] min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-start w-[700px] gap-8">
-        <LargeTitle title={largeTitle} />
-        <Seperator color="bg-[#13171D]" />
-        <MediumTitle
-          title={mediumTitle}
-          className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
-        />
-        <div className="flex flex-col items-start gap-3 mt-[8px]">
-          {creonPassItems.map((item, index) => (
-            <Card key={index}>
-              <span className="font-satoshi text-[18px]">{item}</span>
-            </Card>
-          ))}
+    <div className="w-[90%] md:w-[70%] lg:w-[750px] xl:w-[1075px] 2xl:w-[1450px] min-h-screen flex items-center justify-center">
+      <div className="flex items-center gap-11 ">
+        <CreonPassDetails />
+        <div className="2xl:flex w-full h-full hidden">
+          <Image
+            src="/images/creonPass.png"
+            alt="creonPass"
+            width={808}
+            height={808}
+          />
         </div>
-      </div>
-      <div className="flex h-[808px] w-[808px]">
-        <Image
-          src="/images/creonPass.png"
-          alt="creonPass"
-          width={808}
-          height={808}
-        />
       </div>
     </div>
   );
