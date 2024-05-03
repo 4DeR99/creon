f310149e87a2b7b93bbd1ec3bc5fec81ee32a88f
4DeR99 <moulmado@student.1337.ma>
2024-05-03 13:30:08 +0100

    feat: added the upper section

--- app/components/organisms/MainSection.tsx
+++ app/components/organisms/MainSection.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import Image from 'next/image';
 import backgroundImage from 'images/mainBackground.png';
+import LargeTitle from 'components/atoms/LargeTitle';
+import Seperator from 'components/atoms/Seperator';
+import MediumTitle from 'components/atoms/MediumTitle';
 
 function MainSection() {
+  const largTitle = `The world's first platform for Tokenizing \n AI blockchain projects`;
+  const mediumTitle =
+    'Hold the Creon Pass NFT and earn passive income from AI Tools';
   return (
-    <div className="min-h-screen flex">
+    <div className="min-h-screen flex flex-col items-center justify-end">
       <Image
         src="/images/mainBackground.png"
         alt="background"
         layout="fill"
-        objectFit="cover"
         objectPosition="center"
-        className="absolute top-0 z-10"
+        className="absolute top-0 z-10 object-cover"
       />
-      <div className="text-primaryText text-4xl z-20">zozozozooz</div>
+      <div className="w-full md:w-[80%] lg:w-[750px] xl:w-[1075px] 2xl:w-[1450px] flex flex-col items-start gap-10 text-primaryText mx-auto mb-[10vh] z-20 h-full">
+        <LargeTitle title={largTitle} className="xl:pr-[8rem] pt-1" />
+        <div className="flex flex-col gap-3">
+          <Seperator color="bg-white bg-gradient-to-r from-primary to-secondary" />
+          <MediumTitle
+            title={mediumTitle}
+            className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
+          />
+          <Seperator color="bg-white bg-gradient-to-r from-primary to-secondary" />
+        </div>
+      </div>
     </div>
   );
 }
