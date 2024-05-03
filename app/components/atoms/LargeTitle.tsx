21df8f5f879f3455ec09b15987c5a92beab3df27
4DeR99 <moulmado@student.1337.ma>
2024-05-03 23:19:51 +0100

    chore: update some mobile view fontsize

--- app/components/atoms/LargeTitle.tsx
+++ app/components/atoms/LargeTitle.tsx
@@ -8,7 +8,7 @@ interface LargeTitleProps {
 function LargeTitle({ title, className }: LargeTitleProps) {
   return (
     <h1
-      className={`leading-[1.1] font-monument text-[25px] md:text-[38px] lg:text-[50px] xl:text-[68px] uppercase ${className}`}>
+      className={`leading-[1.1] font-monument text-[28px] md:text-[38px] lg:text-[50px] xl:text-[68px] uppercase ${className}`}>
       {title}
     </h1>
   );
