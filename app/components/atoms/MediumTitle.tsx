113df95f5c5009d96821e8c66def7a5e27dcf4e0
4DeR99 <moulmado@student.1337.ma>
2024-05-04 19:31:16 +0100

    updated titles to accept children

--- app/components/atoms/MediumTitle.tsx
+++ app/components/atoms/MediumTitle.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 interface MediumTitleProps {
+  children?: React.ReactNode;
   title: string;
   className?: string;
 }
 
-function MediumTitle({ title, className }: MediumTitleProps) {
+function MediumTitle({ title, className, children }: MediumTitleProps) {
+  if (children)
+    return (
+      <h2
+        className={`leading-[1.3] font-monument text-[25px] md:text-[38px] uppercase ${className}`}>
+        {children}
+      </h2>
+    );
+
   return (
     <h2
       className={`leading-[1.3] font-monument text-[25px] md:text-[38px] uppercase ${className}`}>
--- app/components/atoms/SmallTitle.tsx
+++ app/components/atoms/SmallTitle.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 interface MediumTitleProps {
+  children?: React.ReactNode;
   title: string;
   className?: string;
 }
 
-function MediumTitle({ title, className }: MediumTitleProps) {
+function MediumTitle({ title, className, children }: MediumTitleProps) {
+  if (children)
+    return (
+      <h2
+        className={`leading-[1.3] font-satoshiBold text-[18px] md:text-[22px] ${className}`}>
+        {children}
+      </h2>
+    );
+
   return (
     <h2
       className={`leading-[1.3] font-satoshiBold text-[18px] md:text-[22px] ${className}`}>
