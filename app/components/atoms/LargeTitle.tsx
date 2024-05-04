8a11257ca82fe583a9f24619b433a24ae7703c8c
4DeR99 <moulmado@student.1337.ma>
2024-05-04 19:29:28 +0100

    chore: updated large title to accept children

--- app/components/atoms/LargeTitle.tsx
+++ app/components/atoms/LargeTitle.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 interface LargeTitleProps {
+  children?: React.ReactNode;
   title: string;
   className?: string;
 }
 
-function LargeTitle({ title, className }: LargeTitleProps) {
+function LargeTitle({ title, className, children }: LargeTitleProps) {
+  if (children) return (
+    <h1
+      className={`leading-[1.1] font-monument text-[28px] md:text-[38px] lg:text-[50px] xl:text-[68px] uppercase ${className}`}>
+      {children}
+    </h1>
+  );
+
   return (
     <h1
       className={`leading-[1.1] font-monument text-[28px] md:text-[38px] lg:text-[50px] xl:text-[68px] uppercase ${className}`}>
