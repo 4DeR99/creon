f5fe07cc735c7707beaa3286fe641ce99ef5e8c3
4DeR99 <moulmado@student.1337.ma>
2024-05-05 16:37:45 +0100

    feat: added new component socials

---
+++ app/components/molecules/Socials.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Image from 'next/image';
+import Telegram from 'assets/icons/telegram.svg';
+import Discord from 'assets/icons/discord.svg';
+import Twitter from 'assets/icons/twitter.svg';
+
+function Socials() {
+  return (
+    <div className="flex gap-2.5">
+      <ImageContainer>
+        <Image src={Telegram} alt="telegram" width={13.33} height={12.44} />
+      </ImageContainer>
+      <ImageContainer>
+        <Image src={Discord} alt="discord" width={15.06} height={12.24} />
+      </ImageContainer>
+      <ImageContainer>
+        <Image src={Twitter} alt="twitter" width={14.67} height={12} />
+      </ImageContainer>
+    </div>
+  );
+}
+
+function ImageContainer({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-[34px] h-[34px] rounded-full border-white border-[2px] flex items-center justify-center hover:bg-primary hover:border-primary transition duration-300">
+      {children}
+    </div>
+  );
+}
+
+export default Socials;
