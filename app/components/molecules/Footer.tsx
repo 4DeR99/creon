07f8cb03873ceb2e8bb79e773ba33bc1c3585cc2
4DeR99 <moulmado@student.1337.ma>
2024-05-05 17:20:28 +0100

    chore: update some tweaks

--- app/components/molecules/Footer.tsx
+++ app/components/molecules/Footer.tsx
@@ -1,13 +1,11 @@
 'use client';
 import React from 'react';
 import Socials from './Socials';
-import Image from 'next/image';
-import Niftables from 'assets/icons/niftables.svg';
 
 function Footer() {
   return (
-    <div className="w-full h-[84px] flex justify-center pt-[15px]">
-      <div className="w-[90%] md:w-[70%] lg:w-[750px] xl:w-[1075px] 2xl:w-[1450px] flex flex-col gap-4 sm:gap-0 sm:flex-row justify-start sm:justify-between">
+    <div className="relative w-full h-[84px] flex justify-center pt-[15px]">
+      <div className="w-[90%] md:w-[70%] lg:w-[750px] xl:w-[1075px] 2xl:w-[1450px] flex flex-col gap-4 sm:gap-0 sm:flex-row justify-start sm:justify-between z-50">
         <div className="flex items-center gap-[90px] h-fit">
           <span className="text-[16px] font-satoshi">
             © Creon 2023. All rights reserved.
